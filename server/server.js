34db6c2b72e7ef841701ba88e984ee5c87ee16f3
raunaksingh1806 <55934288+raunaksingh1806@users.noreply.github.com>
2023-07-26 22:13:12 -0300

    added insert endpoint for review table

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+
 
 const mysql = require('mysql');
 const conn = mysql.createConnection({
@@ -50,15 +53,29 @@ app.get('/review', (req, res) => {
         throw err;
       }
       else if (result.length === 0) {
-        res.status(404).send('User not found');
+        res.status(404).send('vendor not found');
       }
       else {
         const reviews = result.map(item => ({
           rating: item.rating,
           heading: item.heading,
-          desc: item.desc
+          description: item.description
         }));
         res.json(reviews);
       }
     });
+});
+
+app.post('/addReview', (req, res) => {
+  console.log(req.body);
+  const sql = 'INSERT INTO vendors_review (vendor_id, rating, heading, description) VALUES (?, ?, ?, ?)';
+  const { vendor_id, rating, heading, description } = req.body;
+  conn.query(sql, [vendor_id, rating, heading, description], (err) => {
+    if(err) {
+      throw err;
+    }
+    else {
+      res.send('review added');
+    }
+  })
 });
